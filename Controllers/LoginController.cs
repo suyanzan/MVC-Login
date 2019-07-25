2fc96e9618c6f6851734f6f54f831f04cb4b219e
suyanzan <suyanzan@gmail.com>
2019-07-25 20:23:26 +0800

    Add files via upload

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DH_MVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(FormCollection post)
+        {
+            string domain = post["domain"];//抓前端id,name屬性也要有
+            string account = post["username1"];//抓前端id
+            string password = post["Password1"];//抓前端id
+
+            //驗證密碼
+            if (CheckPassword(account, password,domain))
+            {
+                Response.Redirect("~/Home/Home");
+                return new EmptyResult();
+            }
+            else
+            {
+                ViewBag.Msg = "登入失敗...";
+                return View();
+            }
+        }
+        public static  bool CheckPassword(string account,string password,string domain)
+        {
+            if (account == "123" && password == "123" && domain == "AS")
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+
+}
---
+++ Controllers/TCBSController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DH_MVC.Controllers
+{
+    public class TCBSController : Controller
+    {
+        // GET: TCBS
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
