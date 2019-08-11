e241e47aeff1642aacc812d006e8e5a8525f3984
suyanzan <suyanzan@gmail.com>
2019-08-11 21:47:57 +0800

    Add files via upload

---
+++ Models/TCBSs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DH_MVC.Models
+{
+    public class TCBSs
+    {
+        public string Product { get; set; }
+        public string Version { get; set; }
+        public string DPF{ get; set; }
+        public string FileList { get; set; }
+    }
+}
